64d1cf114837635451c1d3e2052cf34263374818
Muhammad Usman <abc.usman.100@gmail.com>
2026-07-08 11:07:06 +0500

    add list card on fontend through API

--- src/Components/Park_Cards.jsx
+++ src/Components/Park_Cards.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Park_Cards() {
+function Park_Cards({ park }) {
   return (
     <div className="rounded-2xl border border-gray-100 shadow-md hover:shadow-xl overflow-hidden bg-gradient-to-b from-white to-emerald-50 flex flex-col transition-shadow">
       <img
-        src="https://images.unsplash.com/photo-1615551043360-33de8b5f410c?w=600"
-        alt="Grand Canyon National Park"
+        src={park.images?.[0]?.url}
+        alt={park.images?.[0]?.altText}
         className="w-full h-60 object-cover"
       />
       <div className="p-5 flex flex-col flex-1">
         <h2 className="font-bold text-xl text-gray-800 mb-4 flex-1">
-          Grand Canyon National Park
+          {park.fullName}
         </h2>
-        <button className="bg-gray-800 hover:bg-gray-900 text-white rounded-full px-4 py-2.5 font-medium transition-colors cursor-pointer">
+        {/* <button className="bg-gray-800 hover:bg-gray-900 text-white rounded-full px-4 py-2.5 font-medium transition-colors cursor-pointer">
           See Details
-        </button>
+        </button> */}
+        <Link
+          to={`/park/${park.id}`}
+          className="bg-gray-800 hover:bg-gray-900 text-white rounded-full px-4 py-2.5 font-medium transition-colors cursor-pointer text-center inline-block"
+        >
+          See Details
+        </Link>
       </div>
     </div>
   );
--- src/Components/Park_List.jsx
+++ src/Components/Park_List.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Park_Cards from './Park_Cards'
 
-function Park_List() {
+function Park_List({parks}) {
+
+    if(parks.length === 0){
+        return <p className="text-center text-gray-500 mt-10">
+        No parks found.
+      </p>
+    }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
           
-          <Park_Cards/>
+          {
+            parks.map((park)=>(
+                
+                <Park_Cards key={(park.id)} park={park}/>
+            ))
+          }
 
         </div>
   )
